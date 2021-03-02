234b5e37029b43aaa51a01977f5bcd9498610bfd
Nikolay <rasich16@gmail.com>
2021-03-02 18:46:30 +0200

    sliders

---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,96 @@
+import Slider from "./slider";
+
+export default class MiniSlider extends Slider {
+  constructor(container, next, prev, activeClass, animate, autoplay) {
+    super(container, next, prev, activeClass, animate, autoplay);
+  }
+
+  decorizeSlide() {
+    this.slides.forEach(slide => {
+      slide.classList.remove(this.activeClass);
+      if (this.animate) {
+        slide.querySelector('.card__title').style.opacity = '0.4';
+        slide.querySelector('.card__controls-arrow').style.opacity = '0';
+      }
+    });
+
+    if (!this.slides[0].closest('button')) {
+      this.slides[0].classList.add(this.activeClass);
+    }
+    
+    if (this.animate) {
+      this.slides[0].querySelector('.card__title').style.opacity = '1';
+      this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+    }
+  }
+
+  nextSlide(){
+    for (let i = 1; i < this.slides.length; i++) {
+      if (this.slides[i].tagName !== "BUTTON") {
+        this.container.appendChild(this.slides[0]);
+        this.decorizeSlide();        
+        break;
+      } else {
+        this.container.appendChild(this.slides[0]);
+        i--;
+      }
+    }
+  }
+
+  bindTriggers() {
+    this.next.addEventListener('click', () => this.nextSlide());
+    
+    this.prev.addEventListener('click', () => {
+      
+      for (let i = this.slides.length - 1; i > 0; i--) {
+        if (this.slides[i].tagName !== "BUTTON") {
+          let active = this.slides[i];
+          this.container.insertBefore(active, this.slides[0]);
+          this.decorizeSlide();
+          break;  
+        }
+      }
+    });
+  }
+
+  autoPlayStop() {
+    let autoplay = setInterval(() => this.nextSlide(), 5000);
+
+    this.container.addEventListener('mouseenter', () => {
+      clearInterval(autoplay);
+    });
+    this.next.addEventListener('mouseenter', () => {
+      clearInterval(autoplay);
+    });
+    this.prev.addEventListener('mouseenter', () => {
+      clearInterval(autoplay);
+    });
+  }
+
+  init() {
+    this.container.style.cssText = `
+      display: flex;
+      flex-wrap: wrap;
+      overflow: hidden;
+      align-items: flex-start;
+    `;
+
+    this.bindTriggers();
+    this.decorizeSlide();
+
+    if (this.autoplay) {
+      this.autoPlayStop();
+    }
+    this.autoPlayStop();
+
+    this.container.addEventListener('mouseleave', () => {
+      this.autoPlayStop();
+    });
+    this.next.addEventListener('mouseleave', () => {
+      this.autoPlayStop();
+    });
+    this.prev.addEventListener('mouseleave', () => {
+      this.autoPlayStop();
+    });
+  }
+}
